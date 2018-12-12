28d7ffae2de66a322eb389e2cf0108522aa27574
Philip Niedertscheider <phil@techprimate.com>
2018-12-12 16:16:44 +0100

    Improved arc calculations

--- Source/Classes/Parser/Elements/Path Instructions/TPSVGInstrEllipticalArcCurve.swift
+++ Source/Classes/Parser/Elements/Path Instructions/TPSVGInstrEllipticalArcCurve.swift
@@ -44,7 +44,8 @@ class TPSVGInstrEllipticalArcCurve: TPSVGInstruction {
     /**
      TODO: Add documentation
      */
-    init(radius: CGVector, xAxisRotation: CGFloat, largeArcFlag: Bool, sweepFlag: Bool, end: CGPoint, relative: Bool = false) {
+    init(radius: CGVector, xAxisRotation: CGFloat, largeArcFlag: Bool,
+         sweepFlag: Bool, end: CGPoint, relative: Bool = false) {
         self.end = end
         self.radius = radius
         self.xAxisRotation = xAxisRotation
@@ -55,18 +56,14 @@ class TPSVGInstrEllipticalArcCurve: TPSVGInstruction {
 
     // MARK: - CustomStringConvertible
 
-    /**
-     TODO: Add documentation
-     */
+    /// :nodoc:
     override var description: String {
         return "TPSVGInstrEllipticalArcCurve {}"
     }
 
     // MARK: - CustomDebugStringConvertible
 
-    /**
-     TODO: Add documentation
-     */
+    /// :nodoc:
     override var debugDescription: String {
         return "TPSVGInstrEllipticalArcCurve { radius: \(radius), x-axis-rotation: \(xAxisRotation), "
             + "largeArcFlag: \(largeArcFlag), sweepFlag: \(sweepFlag), end: \(end), relative: \(relative) }"
@@ -79,30 +76,20 @@ class TPSVGInstrEllipticalArcCurve: TPSVGInstruction {
      */
     // swiftlint:disable identifier_name
     override func modify(path: CGMutablePath, prev: TPSVGInstruction?, prevStartPoint: CGPoint?) {
-        let start = path.isEmpty ? .zero : path.currentPoint
-        let end = self.end
-        var r = self.radius
-        let xAxisRotation = self.xAxisRotation
-        let largeArcFlag = self.largeArcFlag
-        let sweepFlag = self.sweepFlag
-        let c = end + (relative ? start : .zero)
-
         let tau = CGFloat.pi * 2.0
-
         let phi: (sin: CGFloat, cos: CGFloat) = (sin: sin(xAxisRotation / 360 * tau),
                                                  cos: cos(xAxisRotation / 360 * tau))
 
+        let start = path.isEmpty ? .zero : path.currentPoint
+        let c = end + (relative ? start : .zero)
         let pp = CGPoint(x: phi.cos * (start.x - c.x) / 2 + phi.sin * (start.y - c.y) / 2,
                          y: -phi.sin * (start.x - c.x) / 2 + phi.cos * (start.y - c.y) / 2)
-
-        if pp == .zero {
+        guard pp != .zero else {
             return
         }
 
-        r = abs(r)
-
-        let lambda: CGFloat = pow(pp.x, 2) / pow(r.dx, 2) + pow(pp.y, 2) / pow(r.dy, 2)
-
+        var r = abs(radius)
+        let lambda = pow(pp.x, 2) / pow(r.dx, 2) + pow(pp.y, 2) / pow(r.dy, 2)
         if lambda > 1 {
             r *= sqrt(lambda)
         }
@@ -110,24 +97,13 @@ class TPSVGInstrEllipticalArcCurve: TPSVGInstruction {
         let rSq = pow(r, 2)
         let ppSq = pow(pp, 2)
 
-        var radicant = (rSq.dx * rSq.dy) - (rSq.dx * ppSq.y) - (rSq.dy * ppSq.x)
-        if radicant < 0 {
-            radicant = 0
-        }
-
+        var radicant = max((rSq.dx * rSq.dy) - (rSq.dx * ppSq.y) - (rSq.dy * ppSq.x), 0)
         radicant /= (rSq.dx * ppSq.y) + (rSq.dy * ppSq.x)
         radicant = sqrt(radicant) * (largeArcFlag == sweepFlag ? -1 : 1)
 
-        let centerP = CGPoint(x: radicant * r.dx / r.dy * pp.y,
-                              y: radicant * -r.dy / r.dx * pp.x)
-
-        let center = CGPoint(x: phi.cos * centerP.x - phi.sin * centerP.y + (start.x + c.x) / 2,
-                             y: phi.sin * centerP.x + phi.cos * centerP.y + (start.y + c.y) / 2)
-
-        let v1 = CGPoint(x: (pp.x - centerP.x) / rSq.dx,
-                         y: (pp.y - centerP.y) / rSq.dy)
-        let v2 = CGPoint(x: (-pp.x - centerP.x) / rSq.dx,
-                         y: (-pp.y - centerP.y) / rSq.dy)
+        let centerP = CGPoint(x: radicant * r.dx / r.dy * pp.y, y: radicant * -r.dy / r.dx * pp.x)
+        let v1 = CGPoint(x: (pp.x - centerP.x) / rSq.dx, y: (pp.y - centerP.y) / rSq.dy)
+        let v2 = CGPoint(x: (-pp.x - centerP.x) / rSq.dx, y: (-pp.y - centerP.y) / rSq.dy)
 
         var ang1 = vectorAngle(u: CGVector(dx: 1, dy: 0), v: CGVector(dx: v1.x, dy: v1.y))
         var ang2 = vectorAngle(u: CGVector(dx: v1.x, dy: v1.y), v: CGVector(dx: v2.x, dy: v2.y))
@@ -135,32 +111,39 @@ class TPSVGInstrEllipticalArcCurve: TPSVGInstruction {
         if sweepFlag == false && ang2 > 0 {
             ang2 -= tau
         }
-
         if sweepFlag == true && ang2 < 0 {
             ang2 += tau
         }
 
         let segments = Int(max(ceil(abs(ang2) / (tau / 4.0)), 1))
-
         ang2 /= CGFloat(segments)
 
+        let center = CGPoint(x: phi.cos * centerP.x - phi.sin * centerP.y + (start.x + c.x) / 2,
+                             y: phi.sin * centerP.x + phi.cos * centerP.y + (start.y + c.y) / 2)
+
         for _ in 0..<segments {
-            let factor: CGFloat = 4.0 / 3.0 * tan(ang2 / 4.0)
-
-            let point1 = CGPoint(x: cos(ang1), y: sin(ang1))
-            let point2 = CGPoint(x: cos(ang1 + ang2), y: sin(ang1 + ang2))
-
-            let p1 = mapToEllipse(point: CGPoint(x: point1.x - point1.y * factor,
-                                                 y: point1.y + point1.x * factor),
-                                  radius: r, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
-            let p2 = mapToEllipse(point: CGPoint(x: point2.x + point2.y * factor,
-                                                 y: point2.y - point2.x * factor),
-                                  radius: r, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
-            let p  = mapToEllipse(point: point2, radius: r, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
-
-            path.addCurve(to: p, control1: p1, control2: p2)
+            calcCurve(to: path, center: center, phi: phi, radius: r, ang1: ang1, ang2: ang2)
             ang1 += ang2
         }
+    }
+
+    /**
+     Calculates the control and end points using the given arc parameters and adds it to the path.
+     */
+    private func calcCurve(to path: CGMutablePath, center: CGPoint, phi: (sin: CGFloat, cos: CGFloat),
+                           radius: CGVector, ang1: CGFloat, ang2: CGFloat) {
+        let factor: CGFloat = 4.0 / 3.0 * tan(ang2 / 4.0)
+
+        let point1 = CGPoint(x: cos(ang1), y: sin(ang1))
+        let point2 = CGPoint(x: cos(ang1 + ang2), y: sin(ang1 + ang2))
+
+        let p1 = mapToEllipse(point: CGPoint(x: point1.x - point1.y * factor, y: point1.y + point1.x * factor),
+                              radius: radius, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
+        let p2 = mapToEllipse(point: CGPoint(x: point2.x + point2.y * factor, y: point2.y - point2.x * factor),
+                              radius: radius, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
+        let p  = mapToEllipse(point: point2, radius: radius, cosPhi: phi.cos, sinPhi: phi.sin, center: center)
+
+        path.addCurve(to: p, control1: p1, control2: p2)
     }
 
     /**
